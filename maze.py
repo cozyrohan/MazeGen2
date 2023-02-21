6ee62d9a11a7326f0091937621b33a815fe2b4ad
cozyrohan <rohan.jyrm@gmail.com>
2023-02-21 11:54:39 -0800

    backtracking solver working

--- maze.py
+++ maze.py
@@ -56,55 +56,77 @@ def do_random_prims(grid_x_squares, grid_y_squares):
     start_x, start_y = 1,1
     #randomPrims.do_randomPrims(grid_x_squares, grid_y_squares)
     return [m for m in randomPrims.do_prims(grid_x_squares, grid_y_squares)]
-        
+
+
 def draw_maze_using_prims(grid_start_x, grid_start_y, maze_width, maze_height, maze = None):
     #maze = do_random_prims(gridManager.GRID_X, gridManager.GRID_Y)
     for x,y,dir in maze:
         w,h = int(maze_width/gridManager.GRID_X), int(maze_height/gridManager.GRID_Y)
         r = MOVES_DICT[dir](grid_start_x + (x * w) , grid_start_y + (y * h) , w - 2, h - 2)
         yield r
+
+def solve_random_prims():
+    return randomPrims.maze_solve()
+
+def draw_maze_solution(grid_start_x, grid_start_y, maze_width, maze_height, solution):
+    for x, y, dir in solution:
+        if dir == 'X' : break
+        w,h = int(maze_width/gridManager.GRID_X), int(maze_height/gridManager.GRID_Y)
+        r = MOVES_DICT[dir](grid_start_x + (x * w) , grid_start_y + (y * h) , w - 2, h - 2)
+        yield r   
     
 
-
-
-if __name__ == '__main__':
+def start_pygame():
     print('Welcome to the maze generator/solver.')
     pygame.init()
-
     screen = pygame.display.set_mode((800, 800), pygame.RESIZABLE)
     screen.fill(PURPLE)
-
     maze_backdrop = activeBG.init_maze_bg_rect(800, 800)
+    pygame.display.update()
+    return screen, maze_backdrop
     
+if __name__ == '__main__':
+    screen, maze_backdrop = start_pygame()
     running = True
     drawn = False
     mz = do_random_prims(gridManager.GRID_X, gridManager.GRID_Y)
+    solution_path = solve_random_prims()
     while running:
-        screen.fill(DESERT_TAN)
 
         WINDOW_WIDTH, WINDOW_HEIGHT =  screen.get_size()
         
         grid_start_x, grid_start_y, backdrop_width, backdrop_height = activeBG.update_maze_bg_rect(maze_backdrop, WINDOW_WIDTH, WINDOW_HEIGHT)
-        pygame.draw.rect(screen, DESERT_TAN, maze_backdrop) 
         
-        print("grid_start_x, grid_start_y,",  grid_start_x, grid_start_y, 
-              "WINDOW_WIDTH, WINDOW_HEIGHT",  WINDOW_WIDTH, WINDOW_HEIGHT,
-              "backdrop_width, backdrop_height",  backdrop_width, backdrop_height)
+        #print("grid_start_x, grid_start_y,",  grid_start_x, grid_start_y, 
+        #      "WINDOW_WIDTH, WINDOW_HEIGHT",  WINDOW_WIDTH, WINDOW_HEIGHT,
+        #      "backdrop_width, backdrop_height",  backdrop_width, backdrop_height)
 
 
+        screen.fill(DESERT_TAN)
+        #if not drawn:
         for sq in gridManager.make_grid(grid_start_x, grid_start_y, backdrop_width, backdrop_height):
             pygame.draw.rect(screen, PURPLE, sq, width=1 ) 
-
+        pygame.display.update()
+        pygame.time.wait(1000)
         for sq in gridManager.make_wall_border(grid_start_x, grid_start_y, backdrop_width, backdrop_height):
             pygame.draw.rect(screen, PURPLE, sq) 
+        pygame.display.update()
+        pygame.time.wait(1000)
 
-        for sq in draw_maze_using_prims(grid_start_x, grid_start_y, backdrop_width, backdrop_height, maze = mz):
+        for sq in list(draw_maze_using_prims(grid_start_x, grid_start_y, backdrop_width, backdrop_height, maze = mz)):
             pygame.draw.rect(screen, DESERT_TAN, sq) 
+            #print("drew a rect")
             pygame.display.update()
             pygame.time.wait(4)
+
+        for sq in draw_maze_solution(grid_start_x, grid_start_y, backdrop_width, backdrop_height, solution = solution_path):
+            pygame.draw.rect(screen, LIGHT_GREEN, sq) 
+            pygame.display.update()
+            pygame.time.wait(4)
+
+        
             drawn = True
 
-        pygame.display.update()
 
         for event in pygame.event.get():
                 if event.type == pygame.QUIT:
--- randomPrims.py
+++ randomPrims.py
@@ -21,17 +21,44 @@ Plan of action:
     } repeat till done
 
 '''
+
 from random import shuffle
+import gridManager
+class GridSpace:
+    def __init__(self, x, y, status):
+        self.x_coord = x
+        self.y_coord = y
+        self.status = status   #unvisited, wall, or a number
+        self.distance_from_start = 0 
+        self.rect_direction = 'X'
+    def set_status(self, stat):
+        self.status = stat
+    def get_status(self):
+        return self.status
+    def get_distance_from_start(self):
+        return self.distance_from_start
+    def increment_distance_from_prev(self, prev):
+        self.distance_from_start +=  prev + 1
+    def get_rect_dir(self):
+        return self.rect_direction
+    def set_rect_dir(self, dir):
+        self.rect_direction = dir
+    def get_backtrack_rect(self):
+        return (self.x_coord, self.y_coord, self.rect_direction)
 
-def print_status(status):
+def print_status(status = None):
+    #status = [[ GridSpace(i, j, 'unvisited') for j in range(10)] for i in range(10)]
     for row in status:
-        print('\n', row)
+        msg = ''
+        for space in row:
+            msg += (str(space.get_rect_dir()) + str(space.get_status()) + str(space.get_distance_from_start())).center(13)
+        print(msg)
 
-def account_border(grid_x_squares, grid_y_squares, status):
-    for i in range(grid_x_squares):
-        for j in range(grid_y_squares):
-            if i == 0 or i == grid_y_squares-1 or j == 0 or j == grid_x_squares - 1:
-                status[i][j] = 3
+def account_border(GRID_X_SQUARES, GRID_Y_SQUARES, status):
+    for i in range(GRID_X_SQUARES):
+        for j in range(GRID_Y_SQUARES):
+            if i == 0 or i == GRID_Y_SQUARES-1 or j == 0 or j == GRID_X_SQUARES - 1:
+                status[i][j].set_status("wall")
     
     return status
 
@@ -45,38 +72,51 @@ def move_west(cur_x, cur_y):
     return cur_x-1, cur_y
 
 ALL_DIRS = { ("N", move_north), ("S", move_south), ("E", move_east), ("W", move_west) }
+ALL_DIRS_DICT = { "N": move_north, "S": move_south, "E": move_east, "W": move_west }
+
 OPPOSITE = { "N":"S", "S":"N", "E":"W", "W":"E"}
 
 def square_unvisited(x, y, status):
-    return status[y][x] == 0
+    return status[y][x].get_status() == "unvisited"
 def square_frontier(x, y, status):
-    return status[y][x] == 1
+    return status[y][x].get_status() == "frontier"
 def square_visited(x, y, status):
-    return status[y][x] == 2
+    return status[y][x].get_status() == "visited" 
 def square_wall(x, y, status):
-    return status[y][x] == 3
+    return status[y][x].get_status() == "wall" 
 
+GRID_X_SQUARES = gridManager.GRID_X
+GRID_Y_SQUARES = gridManager.GRID_Y
 
-def do_prims(grid_x_squares, grid_y_squares):
+grid = [[ GridSpace(j, i, 'unvisited') for j in range(gridManager.GRID_Y)] for i in range(gridManager.GRID_X)]
+
+def do_prims(GRID_X_SQUARES, GRID_Y_SQUARES):
     
-    status = [[ 0 for j in range(grid_y_squares)] for i in range(grid_x_squares)]
-    account_border(grid_x_squares, grid_y_squares, status)
+    account_border(GRID_X_SQUARES, GRID_Y_SQUARES, grid)
     
+    #print_status(grid)
     
     cur_x, cur_y = (1,1)
 
-    status[cur_y][cur_x] = 2
+
+    #index  row , col
+    grid[cur_y][cur_x].set_status('visited')
+
 
     frontier_structure = set()
+    nth_step = 1
+
     while(True):
         #add all adjecent squares to cur to the frontier set
         for dir, move_func in ALL_DIRS:
             next_x, next_y = move_func(cur_x, cur_y)
-            if square_unvisited(next_x, next_y, status):
+            if square_unvisited(next_x, next_y, grid):
                 frontier_structure.add((next_x, next_y))
-                status[next_y][next_x] = 1
+                #grid[next_y][next_x].increment_distance_from_prev( grid[cur_y][cur_x].get_distance_from_start())
+                grid[next_y][next_x].set_status('frontier')
 
-        #print_status(status)
+
+        #print_status(grid)
         #print()
         #now from the frontier set, choose a random member
         if len(frontier_structure) == 0:
@@ -86,24 +126,44 @@ def do_prims(grid_x_squares, grid_y_squares):
         shuffle(ADL)
         #now, randomly, see where the nearest visited part is, then draw from frontier to visited
         for dir, move_func in ADL:
-            next_x, next_y = move_func(front_x, front_y)
-            if square_visited(next_x, next_y, status):
-                status[front_y][front_x] = 2
+            prev_x, prev_y = move_func(front_x, front_y)
+            if square_visited(prev_x, prev_y, grid):
+                grid[front_y][front_x].set_status('visited')
+                grid[front_y][front_x].increment_distance_from_prev( grid[prev_y][prev_x].get_distance_from_start())
+                grid[front_y][front_x].set_rect_dir(dir)
+
                 yield (front_x, front_y, dir)
                 break
         cur_x, cur_y = front_x, front_y    
-        #print_status(status)
+        nth_step += 1
+        #print_status(grid)
         #print()
 
-    return status
+    return grid
     
    
+def maze_solve():
+    print("solving maze")
+    path = []
+    cur_space = grid[GRID_Y_SQUARES-2][GRID_X_SQUARES-2]
+    while(True):
+        print("inloop")
+        path.append(cur_space.get_backtrack_rect())
+
+        dir = cur_space.get_rect_dir()
+        if dir == 'X': break
+
+        next_x, next_y = ALL_DIRS_DICT[dir](cur_space.x_coord, cur_space.y_coord)
+        
+        cur_space = grid[next_y][next_x]
     
+    print(path)
+    return path
+
+
 
 
 if __name__ == '__main__':
-    s = do_prims(10,10)
-    print("DONE")
-    for row in s:
-        print(row)
-    
+    g = do_prims(GRID_X_SQUARES,GRID_Y_SQUARES) 
+    print_status(g)
+    maze_solve()  
